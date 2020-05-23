02b84b3fa0f8b5b659818744c021dcc0df11ed1c
Nick Tyler <tylerns@email.sc.edu>
2020-05-23 12:22:01 +0000

    Update some requirements

--- startup.sh
+++ startup.sh
@@ -2,18 +2,4 @@
 
 pip install -r requirements.txt
 
-jupyter notebook --ip=0.0.0.0 --port=3000 &
-
-pass=$(jupyter notebook list | grep token | cut -d'=' -f2 | cut -d' ' -f1)
-
-
-jupyter notebook list
-echo ""
-echo ""
-echo $pass
-echo ""
-echo ""
-echo ""
-echo ""
-echo ""
-echo ""
+jupyter notebook --ip=0.0.0.0 --port=3000
