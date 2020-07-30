81af8c72316813c2ac0966a0a497b6a8143b8f0c
Justin Roberts-Pierel <jr23@email.sc.edu>
2020-07-30 17:57:22 -0400

    added package example

---
+++ 02_Week2/Workshop_Packages/make_installable.py
@@ -0,0 +1,7 @@
+import codetopackage
+codetopackage.create_package(
+    PackageTargetDirectory= 'stem_package',
+    PackageSourceDirectory= 'myCode',
+    PackageName= "stem_package",
+    CreateDocs=False
+    )
---
+++ 02_Week2/Workshop_Packages/myCode/hello.py
@@ -0,0 +1,3 @@
+
+def test_function():
+    print('Hello World')
